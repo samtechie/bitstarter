40b50688b546c712aec10422d7bbd717623ea397
samtechie <latimerscope@gmail.com>
2013-07-12 16:16:10 +0000

    tried buffer stuff

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var index = fs.readFileSync('/bitstarter/index.html');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf-8',0,index));
 });
 
 var port = process.env.PORT || 5000;
