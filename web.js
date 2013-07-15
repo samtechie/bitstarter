22fade12026c5bd62c80cf93fb8a59aac98bddff
samtechie <latimerscope@gmail.com>
2013-07-15 08:12:26 +0000

    Tried again

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var buffer = new Buffer(10);
-response.send(buffer.toString(fs.readFileSync('index.html')
-));
+buffer = fs.readFileSync('index.html');
+var string1 = buffer.toString();
+response.send(string1);
 });
 
 var port = process.env.PORT || 5000;
