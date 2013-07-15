3d25c7681493782158f8a4d9647a3ba232e62ef2
samtechie <latimerscope@gmail.com>
2013-07-15 12:02:56 +0000

    Fixed some errors

--- grader.js
+++ grader.js
@@ -69,9 +69,9 @@ var clone = function(fn){
 
 if(require.main === module) {
     program
-       .option('-c, --checks <check_file>','Path to checks.json', clone(assertFileExists), CHECKSFILR_DEFAULT)
+       .option('-c, --checks <check_file>','Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists),HTMLFILE_DEFAULT)
-       .process(process.argv);
+       .parse(process.argv);
  var checkJson = checkHtmlFile(program.file,program.checks);
  var outJson =JSON.stringify(checkJson, null, 4);
   console.log(outJson);
